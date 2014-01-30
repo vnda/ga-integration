e849142f78f63e87bece8d76803d658112410441
Luiz Paulo de Farias Jr <luizpaulo@inf.ufsc.br>
2014-01-30 20:39:24 -0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gaintegration::Application.config.secret_key_base = 'f796a6fc3adab78341de1c1a7e73c7ff63fc081415ea8ad14839ae209cde1e26a9d41e0d83152e7539877a00875232dab7d163687ee5f77a20b6d002f59060c6'
