215cc8fde867b47480ab72c86d0fc50ecc182e16
Francisco DC <fdc@razaoinfo.com.br>
2014-03-19 10:37:05 -0300

    typo

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,9 +1,9 @@
 if Rails.env.production?
-  VndaBling::Application.config.secret_token = ENV["SECRET_TOKEN"]
+  Gaintegration::Application.config.secret_token = ENV["SECRET_TOKEN"]
 else
   # run: `bin/rake secret_token:generate` if you doesn't have the secret token file yet
   secret_token_file = Rails.root.join("config/.secret-token")
   if secret_token_file.exist?
-    VndaBling::Application.config.secret_token = secret_token_file.read
+    Gaintegration::Application.config.secret_token = secret_token_file.read
   end
 end
