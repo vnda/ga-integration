4c88ba587d5184955d0e723934d7406c7e1b061b
Germano Nicolini <germanogmn1@gmail.com>
2014-06-17 18:20:10 -0300

    removed asset pipeline

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Gaintegration::Application.routes.draw do
+  root 'stores#index'
   resources :stores
 
   post 'sender/send_transaction' => 'sender#send_transaction'
   post 'sender/send_event' => 'sender#send_event'
-  root 'stores#index'
 end
