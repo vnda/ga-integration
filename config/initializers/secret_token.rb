8185f096c1898f1bab86e2c5f2d6a91f63b6c8a2
Germano Nicolini <germanogmn1@gmail.com>
2014-07-21 11:37:34 -0300

    fix warnings

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,6 +4,6 @@ else
   # run: `bin/rake secret_token:generate` if you doesn't have the secret token file yet
   secret_token_file = Rails.root.join("config/.secret-token")
   if secret_token_file.exist?
-    Gaintegration::Application.config.secret_token = secret_token_file.read
+    Gaintegration::Application.config.secret_key_base = secret_token_file.read
   end
 end
