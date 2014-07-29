5d891bb730809e8b7ec35318f9772b9299a25215
Germano Nicolini <germanogmn1@gmail.com>
2014-07-29 12:14:31 -0300

    ignore spaces on google api key

--- config/initializers/ga_report.rb
+++ config/initializers/ga_report.rb
@@ -1,4 +1,4 @@
 GaReport.tap do |config|
-  config.p12_key = Base64.strict_decode64(ENV['GOOGLE_API_P12']) rescue nil
+  config.p12_key = Base64.strict_decode64(ENV['GOOGLE_API_P12'].gsub(/\s+/, ''))
   config.service_account_email = ENV['GOOGLE_API_ACCOUNT_EMAIL']
 end
