8dcf8b1d85801b93660b249b18efeb2c281ec532
Fernando Varani <fervarani@gmail.com>
2014-10-07 12:13:34 -0300

    structuring

--- app/models/stat_hat_sender.rb
+++ app/models/stat_hat_sender.rb
@@ -1,11 +1,11 @@
 class StatHatSender
+  # Example for this sender
+  #HooksService.run("order_confirmed").with({stat_name: 'order confirmed', stat_value: 1, event_type: 'count'})
 
   def initialize(json, store)
-    puts "BREAK"
-    puts json
     @stat_name = json['stat_name']
     @stat_hat_token = store.stat_hat_token
-    @stat_value = json['stat_value'].to_i
+    @stat_value = json['stat_value']
     @event_type = json['event_type']
   end
 
@@ -16,7 +16,5 @@ class StatHatSender
     end
     Rails.logger.debug("Stathat Event sent: #{@stat_name}")
   end
-# Example for this sender
-#HooksService.run("order_confirmed").with({stat_name: 'order confirmed', stat_value: 1, event_type: 'count'})
 
 end
