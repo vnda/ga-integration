92af8c9d1e846236ffa8432c582544941cea8983
Fernando Varani <fervarani@gmail.com>
2015-02-05 11:45:44 -0200

    Change product category_tags

--- app/models/universal_analytics_event_sender.rb
+++ app/models/universal_analytics_event_sender.rb
@@ -74,9 +74,18 @@ class UniversalAnalyticsEventSender
         "#{key}va" => prod['variant_name'],
         "#{key}qt" => prod['quantity'],
         "#{key}ps" => prod['position'],
+        "#{key}ca" => product_category(prod),
       )
     end
     hash.reject { |k, v| v.blank? }.symbolize_keys
+  end
+
+  def product_category(prod)
+    properties = prod['category_tags']
+    properties.each do |p|
+      return p['name'] if p['tag_type'] == "product_category"
+    end
+    return nil
   end
 
   def transaction_data
