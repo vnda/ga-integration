b93a5f74d4a3a627cedd51dc4370e893a5d9037a
Luiz Paulo de Farias Jr <luiz@luizpaulo.me>
2015-07-23 03:23:23 -0300

    .

--- app/services/ga_source_report.rb
+++ app/services/ga_source_report.rb
@@ -36,6 +36,6 @@ class GaSourceReport
 
   def process_data(data)
     headers = data.column_headers.map { |ch| HEADER_NAMES[ch.name] }
-    data.rows.map { |row| headers.zip(row.map(&:to_i)).to_h }
+    data.rows.map { |row| headers.zip(row.map(&:to_s)).to_h }
   end
 end
